db8bb6b6c9023e200196895f57ccc45f4171d004
Sehani Devindi <sehanidevindi@gmail.com>
2021-02-11 17:24:50 +0530

    Receipt Header Created

---
+++ QTS_SimpleBilling/Model/BillingContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QTS_SimpleBilling.Model
+{
+    internal class BillingContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Data Source=LAPTOP-DM8ANEE6\SQLEXPRESS;Initial Catalog=billing;Integrated Security=True");
+        }
+        public DbSet<ReceiptHeader> ReceiptHeaders { get; set; }
+    }
+}
+
